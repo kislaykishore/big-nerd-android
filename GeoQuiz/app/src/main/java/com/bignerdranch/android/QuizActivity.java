38469d716046fcbe132eab86155935a5ccb60a3a
Kislay Kishore <kislay.kishore2003@gmail.com>
2019-06-22 01:54:28 +0530

    Add landscape layout.
    Preserve the question index

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/QuizActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +13,9 @@ import com.bignerdranch.android.geoquiz.Question;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+
+    private static final String KEY_INDEX = "index";
     private Button mTrueButton;
 
     private Button mFalseButton;
@@ -35,6 +39,10 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = findViewById(R.id.true_button);
@@ -67,6 +75,40 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        bundle.putInt(KEY_INDEX, mCurrentIndex);
+    }
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
